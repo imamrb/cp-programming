804dc7dd4f6c0ddea4e16dfe6cfcb4c6c92d4b95
Imam Hossain <ihossain@dataguar.de>
2023-02-12 02:51:51 +0600

    Restructure some directories

--- 2_interview_preparation/BJIT/README.md
+++
@@ -1,3 +0,0 @@
-## BJIT Interview Problems
-
-Solutions of the Interview questions taken from BJIT Glassdoor profile.
--- concepts/Pointer/2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char *argv[1])
-{
-    double pounds;
-
-    pounds = atof(argv[0]) / 16.0;
-    printf("%f pounds", pounds);
-}
